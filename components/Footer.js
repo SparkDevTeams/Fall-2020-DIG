53474873a4a5dd6f7389428d085ace438c7a7e3a
Angelo Martinez <amart670@fiu.edu>
2020-10-15 20:49:10 -0400

    added functional component

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import footerImg from '../images/footer-image.svg';
 
-export default class Footer extends React.Component {
-  // Logic goes here
-  render() {
-    return (
-      <footer className='footer text-center py-3'>
-        <div className='container'>
-          <img src={footerImg} alt='holding earth' />
-          <p>Made with 💚️ by SparkDev FIU</p>
-        </div>
-      </footer>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer className='footer text-center py-3'>
+      <div className='container'>
+        <img src={footerImg} alt='holding earth' />
+        <p>Made with 💚️ by SparkDev FIU</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
