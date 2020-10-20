2c1c34dc06609ff9346e2514070911949c94f6ff
Danny393 <danielacosta393@gmail.com>
2020-10-20 19:45:13 -0400

    Added NavBar to pages and fixed log-in screen css

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import NavBar from '../components/Navbar';
 
 const Survey = () => {
   return (
     <div>
+      <NavBar/>
       <div className='container-fluid survey-page'>
         <div className='row survey-row'>
           <div className='col left-side'>
