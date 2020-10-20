fc7b53f0bda1ca8a91cd2e426d304e1da5b8aa7f
Angelo Martinez <amart670@fiu.edu>
2020-10-20 19:12:08 -0400

    added carousel component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,76 +1,38 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import NavBar from '../components/Navbar';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Home = () => {
   return (
     <div>
       <NavBar />
-      <div className='container'>
-        <div
-          id='carouselExampleIndicators'
-          className='carousel slide mb-5'
-          data-ride='carousel'
-        >
-          <ol className='carousel-indicators'>
-            <li
-              data-target='#carouselExampleIndicators'
-              data-slide-to='0'
-              className='active'
-            ></li>
-            <li data-target='#carouselExampleIndicators' data-slide-to='1'></li>
-            <li data-target='#carouselExampleIndicators' data-slide-to='2'></li>
-          </ol>
-          <div className='carousel-inner'>
-            <div className='carousel-item active'>
-              <img
-                className='d-block w-100'
-                src='https://via.placeholder.com/350x150'
-                alt='First slide'
-              />
-            </div>
-            <div className='carousel-item'>
-              <img
-                className='d-block w-100'
-                src='https://via.placeholder.com/350x150'
-                alt='Second slide'
-              />
-            </div>
-            <div className='carousel-item'>
-              <img
-                className='d-block w-100'
-                src='https://via.placeholder.com/350x150'
-                alt='Third slide'
-              />
-            </div>
-          </div>
-          <a
-            className='carousel-control-prev'
-            href='#carouselExampleIndicators'
-            role='button'
-            data-slide='prev'
-          >
-            <span
-              className='carousel-control-prev-icon'
-              aria-hidden='true'
-            ></span>
-            <span className='sr-only'>Previous</span>
-          </a>
-          <a
-            className='carousel-control-next'
-            href='#carouselExampleIndicators'
-            role='button'
-            data-slide='next'
-          >
-            <span
-              className='carousel-control-next-icon'
-              aria-hidden='true'
-            ></span>
-            <span className='sr-only'>Next</span>
-          </a>
-        </div>
+      <Carousel indicators={false}>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='https://via.placeholder.com/350x150'
+            alt='First slide'
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='https://via.placeholder.com/350x150'
+            alt='Third slide'
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className='d-block w-100'
+            src='https://via.placeholder.com/350x150'
+            alt='Third slide'
+          />
+        </Carousel.Item>
+      </Carousel>
 
-        <div className='row mb-5 hero-row-reverse-mobile'>
+      <div className='container'>
+        <div className='row mb-5 mt-5 hero-row-reverse-mobile'>
           <div className='col-lg-6 text-center txt-md-left'>
             <div className='row mb-5'>
               <h1 className='display-4 font-weight-bold'>
