ce9b03123f8d7aca6c4ab525b0fc677b5707dd4d
Angelo Martinez <amart670@fiu.edu>
2020-10-22 21:16:33 -0400

    added guide file

---
+++ temp-questions.js
@@ -0,0 +1,19 @@
+const questions = [
+  {
+    title: 'question 1',
+    value: 5,
+    type: 'slider',
+    options: [],
+    tip: ['tips1', 'tips2'],
+  },
+  {
+    title: 'question 2',
+    value: 10,
+    type: '1-5',
+    tip: '',
+  },
+];
+
+export default questions;
+
+// make sure we can use this on the other pages
