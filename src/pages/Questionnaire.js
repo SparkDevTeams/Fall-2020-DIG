560314b147f9b69d6737e1f441c862be43f06073
Angelo Martinez <amart670@fiu.edu>
2020-10-26 23:59:11 -0400

    added cunter refactored

--- src/pages/Questionnaire.js
+++ src/pages/Questionnaire.js
@@ -7,7 +7,6 @@ const Questionnaire = () => {
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
-    console.log(selectedIndex);
   };
 
   return (
@@ -15,7 +14,10 @@ const Questionnaire = () => {
       <div className='container-fluid'>
         <div className='row questionnaire-row'>
           <div className='col-lg-6 questionnaire-left'></div>
-          <div className='col-lg-6 questionnaire-right'>
+          <div className='col-lg-6 px-md-5 questionnaire-right'>
+            <h2 className='questionnaire-step-counter'>
+              Question {index + 1} of 15
+            </h2>
             <Carousel
               activeIndex={index}
               onSelect={handleSelect}
@@ -27,46 +29,13 @@ const Questionnaire = () => {
               indicators={true}
             >
               <Carousel.Item>
-                <img
-                  className='d-block w-100'
-                  src='holder.js/800x400?text=First slide&bg=373940'
-                  alt='First slide'
-                />
-                <Carousel.Caption>
-                  <h3>First slide label</h3>
-                  <p>
-                    Nulla vitae elit libero, a pharetra augue mollis interdum.
-                  </p>
-                </Carousel.Caption>
+                <h1>First slide</h1>
               </Carousel.Item>
               <Carousel.Item>
-                <img
-                  className='d-block w-100'
-                  src='holder.js/800x400?text=Second slide&bg=282c34'
-                  alt='Second slide'
-                />
-
-                <Carousel.Caption>
-                  <h3>Second slide label</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </p>
-                </Carousel.Caption>
+                <h1>second slide</h1>
               </Carousel.Item>
               <Carousel.Item>
-                <img
-                  className='d-block w-100'
-                  src='holder.js/800x400?text=Third slide&bg=20232a'
-                  alt='Third slide'
-                />
-
-                <Carousel.Caption>
-                  <h3>Third slide label</h3>
-                  <p>
-                    Praesent commodo cursus magna, vel scelerisque nisl
-                    consectetur.
-                  </p>
-                </Carousel.Caption>
+                <h1>third slide</h1>
               </Carousel.Item>
             </Carousel>
           </div>
