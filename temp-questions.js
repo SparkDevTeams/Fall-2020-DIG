eec652b5572cebc58fdeb05e3d24bce8db9ca45a
Nathalia Dias <nathaliadiasregiani@gmail.com>
2020-10-27 20:28:55 -0400

    Added questions with array

--- temp-questions.js
+++ temp-questions.js
@@ -1,18 +1,100 @@
 const questions = [
-  {
-    title: 'question 1',
-    value: 5,
-    type: 'slider',
-    options: [],
-    tip: ['tips1', 'tips2'],
-  },
-  {
-    title: 'question 2',
-    value: 10,
-    type: '1-5',
-    tip: '',
-  },
-];
+{
+  ques: "It's a nice and sunny day outside, do you...:",
+  ans: [
+    "Stay inside (it's really hot and the A/C is nice and cold",
+    "Get together with some friends, maybe you can freshen up at the pool or go get some ice cream!"
+  ]
+},  
+{
+  ques: "You just finished drinking a water bottle, do you...:",
+  ans: [
+    "Throw it in the trash.",
+    "Put it aside for recycling."
+  ]
+},  
+{
+  ques: "Your phone/tablet is starting to get a little too slow, do you...:",
+  ans: [
+    "Try and see if you can get it fixed.",
+    "Get a new one and keep the old one—you never know when you might need it",
+    "Get a new one and donate/sell the older one."
+  ]
+},  
+{
+  ques: "You're hungry so you're gonna go to the kitchen to make a sandwich, do you...:",
+  ans: [
+    "Leave the light on, it's only going to be a minute anyway!",
+    "Turn the light off, just in case."
+  ]
+},  
+{
+  ques: "Today is one of the hottest days in the year! Do you...:",
+  ans: [
+    "Take a hot shower, I can't stand the water when it's cold!",
+    "Take a cold shower, it's pretty hot after all."
+  ]
+},  
+{
+  ques: "It's time for you and your other friends to go to your best friend's house! Do you...:",
+  ans: [
+    "Get there everyone in their respective cars.",
+    "Have one of the parents pick some or all of you up and take you there.",
+    "Bike/walk to his house."
+    
+  ]
+},  
+{
+  ques: "You got a new clothes! But you have some older ones you don't wear anymore, do you...:",
+  ans: [
+    "Toss the old ones out, they're probably too old anyway...",
+    "Try and separate some that could be in better shape and donate them"
+  ]
+},  
+{
+  ques: "How many electronic devices do you own (smartphones, tablets, gaming consoles, computers, etc)",
+  ans: [
+    
+  ]
+},  
+{
+  ques: "When you're going on a vacation, do you... ",
+  ans: [
+    "Travel by plane",
+    "Travel by bus",
+    "Travel by train",
+    "Travel by car",
+    "Other"
+  ]
+},  
+{
+  ques: "When your parent get groceries, do they...:",
+  ans: [
+    "Buy from big chains",
+    "Smaller family owned supermarkets",
+    "Farmer markets"
+  ]
+},  
+{
+  ques: "How many times per week do you eat meat (beef, chicken, pork, etc.)?",
+  ans: [
+    
+  ]
+},  
+{
+  ques: "On average, how long do you keep the lights on in a day?",
+  ans: [
+    
+  ]
+},  
+{
+  ques: "When you're brush your teeth, do you leave the water running?",
+  ans: [
+    "True",
+    "False"
+  ]
+} 
+]
 
 export default questions;
 
