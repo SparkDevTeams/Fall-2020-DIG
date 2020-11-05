9ff76227921fffcf97858a8b158b7b3736b2b8f7
Angelo Martinez <amart670@fiu.edu>
2020-11-04 21:14:01 -0500

    added the tips array

---
+++ src/assets/tips.js
@@ -0,0 +1,71 @@
+import tip0 from '../images/girl-earth.svg'; // this is an example of an import for the images
+
+const tips = [
+  {
+    tip:
+      "Make sure you turn off all electronic devices when not in use and don't forget to turn off the light when leaving your room.",
+    img: tip0,
+  },
+  {
+    tip: '1',
+    img: 'tip1',
+  },
+  {
+    tip: '2',
+    img: 'tip2',
+  },
+  {
+    tip: '3',
+    img: 'tip3',
+  },
+  {
+    tip: '4',
+    img: 'tip4',
+  },
+  {
+    tip: '5',
+    img: 'tip5',
+  },
+  {
+    tip: '6',
+    img: 'tip6',
+  },
+  {
+    tip: '7',
+    img: 'tip7',
+  },
+  {
+    tip: '8',
+    img: 'tip8',
+  },
+  {
+    tip: '9',
+    img: 'tip9',
+  },
+  {
+    tip: '10',
+    img: 'tip10',
+  },
+  {
+    tip: '11',
+    img: 'tip11',
+  },
+  {
+    tip: '12',
+    img: 'tip12',
+  },
+  {
+    tip: '13',
+    img: 'tip13',
+  },
+  {
+    tip: '14',
+    img: 'tip14',
+  },
+  {
+    tip: '15',
+    img: 'tip15',
+  },
+];
+
+export default tips;
