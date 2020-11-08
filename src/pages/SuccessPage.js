c3c44e24004665ec8a1bf2ff862502a7b9f416b6
Angelo Martinez <amart670@fiu.edu>
2020-11-08 16:55:50 -0500

    added score param and 404 page

--- src/pages/SuccessPage.js
+++ src/pages/SuccessPage.js
@@ -8,137 +8,142 @@ import Card from 'react-bootstrap/Card';
 import successImg from '../images/success-image.svg';
 import Button from 'react-bootstrap/esm/Button';
 
-const successPage = () => {
+const SuccessPage = () => {
   return (
     <Container fluid>
-  <Row>
-    <Col></Col>
-    <Col xs={6} align='center'>
-      <div className='success-section'>
-        <img
-          src={successImg}
-          className='success-img'
-          alt='Woman jumping with cellphone in the background along a check mark'
-          
-        />
-      </div>
-    </Col>
-    <Col></Col>
-  </Row>
-  <Row>
-    <Col></Col>
-    <Col xs={6} align='center'>
-      <div className='success-text'>
-        <h1>YOU DID IT!</h1>
-        <p>Thank you for taking the Survey! </p>
-      </div>
-    </Col>
-    <Col></Col>
-  </Row>
-  <Row>
-    <Col></Col>
-    <Col xs={3} align='center'>
-      <div className='success-score-text'>
-        <p>Check your Score!!!</p>
-      </div>
-    
-    </Col>
-    <Col></Col>
-  </Row>
-  <Row>
-    <Col></Col>
-    <Col xs={6} align='center' >
-      <Card className='card-points' border='primary' style={{width: '18rem'}}>
-        <Card.Body>75/100</Card.Body>
-      </Card>
-    </Col>
-    <Col></Col>
-  </Row>
-  <Row>
-    <Col></Col>
-    <Col xs={6} align='center'>
-      <div className='tips-text'>
-        <h4>Check these tips to improve your score</h4>
-      </div>
-    </Col>
-    <Col></Col>
-  </Row>
-  <Row>
-    <Col>
-
-    <Accordion className='accordion-section'>
-      <Card className='accordion-section'>
-          <Accordion.Toggle as={Card.Header}  eventKey="0" className="mb-1">
-            Energy Tips
-          </Accordion.Toggle>
-        <Accordion.Collapse eventKey="0" className='accordion-tips'>
-          <Card.Body>
-            <h6>Switch lights off!</h6>
-            <p>When leaving the room turn off the lights and unplug electronic devices when they are not in use.</p>
-          </Card.Body>
-        </Accordion.Collapse>
-        <Accordion.Collapse eventKey="0" className='accordion-tips'>
-          <Card.Body><h6>Lower your thermostat in winter and raise it in summer.</h6> 
-            <p>Use less air conditioning in the summer; instead opt for fans, which require less electricity. 
-              And check out these other ways to beat the heat without air conditioning.</p>
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-      <Card className='accordion-section'>
-          <Accordion.Toggle as={Card.Header} eventKey="1" className="mb-1">
-            Water Tips
-          </Accordion.Toggle>
-        <Accordion.Collapse eventKey="1" className='accordion-tips'>
-          <Card.Body>
-              <h6>Take shorter showers</h6>
-              <p>A typical shower uses five to ten gallons of water a minute. Limit your showers to the time it takes to soap up, wash down and rise off.</p>
-          </Card.Body>
-        </Accordion.Collapse>
-        <Accordion.Collapse eventKey="1" className='accordion-tips'>
-          <Card.Body>
-            <h6>Turn off the water while brushing your teeth</h6>
-            <p>Before brushing, wet your brush and fill a glass for rinsing your mouth. </p>
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-      <Card className='accordion-section'>
-        <Accordion.Toggle as={Card.Header} eventKey="2" >
-            Waste Tips
-          </Accordion.Toggle>
-        <Accordion.Collapse eventKey="2" className='accordion-tips'>
-          <Card.Body>
-            <h6>Reduce your food waste</h6>
-            <p>Plan your meals ahead of time, freeze the excess and reuse leftovers.</p>
-          </Card.Body>
-        </Accordion.Collapse>
-        <Accordion.Collapse eventKey="2" className='accordion-tips'>
-          <Card.Body>
-            <h6>Eat low on the food chain</h6>
-            <p>This means eating mostly fruits, veggies, grains, and beans. </p>
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-</Accordion>
- 
-    </Col>
-    
-  </Row>
- 
-
-</Container>
-
-
-
-
-
-
-
-
-
-
-
-
-
+      <Row>
+        <Col></Col>
+        <Col xs={6} align='center'>
+          <div className='success-section'>
+            <img
+              src={successImg}
+              className='success-img'
+              alt='Woman jumping with cellphone in the background along a check mark'
+            />
+          </div>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col xs={6} align='center'>
+          <div className='success-text'>
+            <h1>YOU DID IT!</h1>
+            <p>Thank you for taking the Survey! </p>
+          </div>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col xs={3} align='center'>
+          <div className='success-score-text'>
+            <p>Check your Score!!!</p>
+          </div>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col xs={6} align='center'>
+          <Card
+            className='card-points'
+            border='primary'
+            style={{ width: '18rem' }}
+          >
+            <Card.Body>75/100</Card.Body>
+          </Card>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col xs={6} align='center'>
+          <div className='tips-text'>
+            <h4>Check these tips to improve your score</h4>
+          </div>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col>
+          <Accordion className='accordion-section'>
+            <Card className='accordion-section'>
+              <Accordion.Toggle as={Card.Header} eventKey='0' className='mb-1'>
+                Energy Tips
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey='0' className='accordion-tips'>
+                <Card.Body>
+                  <h6>Switch lights off!</h6>
+                  <p>
+                    When leaving the room turn off the lights and unplug
+                    electronic devices when they are not in use.
+                  </p>
+                </Card.Body>
+              </Accordion.Collapse>
+              <Accordion.Collapse eventKey='0' className='accordion-tips'>
+                <Card.Body>
+                  <h6>
+                    Lower your thermostat in winter and raise it in summer.
+                  </h6>
+                  <p>
+                    Use less air conditioning in the summer; instead opt for
+                    fans, which require less electricity. And check out these
+                    other ways to beat the heat without air conditioning.
+                  </p>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card className='accordion-section'>
+              <Accordion.Toggle as={Card.Header} eventKey='1' className='mb-1'>
+                Water Tips
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey='1' className='accordion-tips'>
+                <Card.Body>
+                  <h6>Take shorter showers</h6>
+                  <p>
+                    A typical shower uses five to ten gallons of water a minute.
+                    Limit your showers to the time it takes to soap up, wash
+                    down and rise off.
+                  </p>
+                </Card.Body>
+              </Accordion.Collapse>
+              <Accordion.Collapse eventKey='1' className='accordion-tips'>
+                <Card.Body>
+                  <h6>Turn off the water while brushing your teeth</h6>
+                  <p>
+                    Before brushing, wet your brush and fill a glass for rinsing
+                    your mouth.{' '}
+                  </p>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card className='accordion-section'>
+              <Accordion.Toggle as={Card.Header} eventKey='2'>
+                Waste Tips
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey='2' className='accordion-tips'>
+                <Card.Body>
+                  <h6>Reduce your food waste</h6>
+                  <p>
+                    Plan your meals ahead of time, freeze the excess and reuse
+                    leftovers.
+                  </p>
+                </Card.Body>
+              </Accordion.Collapse>
+              <Accordion.Collapse eventKey='2' className='accordion-tips'>
+                <Card.Body>
+                  <h6>Eat low on the food chain</h6>
+                  <p>
+                    This means eating mostly fruits, veggies, grains, and beans.{' '}
+                  </p>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        </Col>
+      </Row>
+    </Container>
 
     // <div className='container'>
     //   <div className='row mb-5 mt-5 hero-row-reverse-mobile' align='center'>
@@ -146,8 +151,7 @@ const successPage = () => {
     //   </div>
     //   <div className='row mb-5 mt-5 hero-row-reverse-mobile'>
     //   <div className='col-lg-6 text-center txt-md-left'>
-                        
-              
+
     //       <div className='col-lg-6'>
     //         <div className='hero-img-right'>
     //           <img
@@ -157,9 +161,7 @@ const successPage = () => {
     //     </div>
     //   </div>
     // </div>
-      
-    
   );
 };
 
-export default successPage;
+export default SuccessPage;
