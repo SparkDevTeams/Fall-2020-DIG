a357df5c25f07f17cd65cee617454c4897af0343
Angelo Martinez <amart670@fiu.edu>
2020-11-09 20:16:14 -0500

    added browserslist to package.json

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,37 +1,34 @@
 import React, { useRef, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
-import { useAuth } from '../states/userState'
+import { useAuth } from '../states/userState';
 import Alert from 'react-bootstrap/Alert';
 
 const LogIn = () => {
-
   //same logic as sign up except function called
   const emailRef = useRef();
   const passwordRef = useRef();
   const { login } = useAuth();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
-  const redirect = useHistory()
+  const redirect = useHistory();
 
-  async function handleLogIn(event){
-    event.preventDefault()
+  async function handleLogIn(event) {
+    event.preventDefault();
 
-    if(passwordRef.current.value === "" ||
-        emailRef.current.value === ""){
-          return setError("Please fill in all fields")
+    if (passwordRef.current.value === '' || emailRef.current.value === '') {
+      return setError('Please fill in all fields');
     }
-    
-    setLoading(true)
-    try{
-      setError("")
-      await login(emailRef.current.value, passwordRef.current.value)
-      redirect.push("/questionnaire")
+
+    try {
+      setError('');
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      redirect.push('/questionnaire');
+    } catch (e) {
+      setError('Failed to sign in');
     }
-    catch (exception){
-      setError("Failed to sign in")
-    }
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
@@ -39,7 +36,7 @@ const LogIn = () => {
       <Form onSubmit={handleLogIn} className='log-in-form'>
         <div className='form-group mb-2'>
           <h3 className='mb-2 text-center font-weight-bold'>Log in</h3>
-          {error && <Alert variant="danger">{error}</Alert>}
+          {error && <Alert variant='danger'>{error}</Alert>}
           <Form.Control
             type='email'
             className='form-control mb-2'
@@ -52,10 +49,14 @@ const LogIn = () => {
             placeholder='Password'
             ref={passwordRef}
           />
-          <button type='submit' disabled={loading} className='btn btn-secondary mt-3 mb-2 btn-block'>
+          <button
+            type='submit'
+            disabled={loading}
+            className='btn btn-secondary mt-3 mb-2 btn-block'
+          >
             Log In
           </button>
-          <div className="text-center">
+          <div className='text-center'>
             Need an account? <Link to='/sign-up'>Sign Up</Link>
           </div>
         </div>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,10 @@
 import React, { useRef, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import { useAuth } from '../states/userState';
 import Alert from 'react-bootstrap/Alert';
 
 const SignUp = () => {
-
   //variables to keep track of input
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -15,47 +14,49 @@ const SignUp = () => {
   const { signup } = useAuth();
 
   //state to keep track of current error to display to the user
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   //loading state to make sure that user does not accidentally try to make more than one
   //user while the current code for signing up is running
-  const[loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   //hook that lets us redirct user to a given page using push()
   const redirect = useHistory();
 
   //function to get all values from the form and try to make an account for the user
-  function handleSignUp(event){
-    event.preventDefault()
-    
+  async function handleSignUp(event) {
+    event.preventDefault();
+
     //check that none of the feilds are empty
-    if(passwordRef.current.value === "" || 
-       passwordConfirmRef.current.value === "" || 
-       emailRef.current.value == ""){
-      return setError("Please fill in all fields")
+    if (
+      passwordRef.current.value === '' ||
+      passwordConfirmRef.current.value === '' ||
+      emailRef.current.value == ''
+    ) {
+      return setError('Please fill in all fields');
     }
 
     //if the passwords do not match then state an error
-    if(passwordRef.current.value !== passwordConfirmRef.current.value){
-      return setError("Passwords do not match")
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Passwords do not match');
     }
-    
+
     //there is a problem here, the handleSignUp function needs to be async so that
     //we can wait for the signup API call to finish and check for any errors
     //something is restricting that, otherwise this works
-    setLoading(true)
-    try{
-      //is all self check are working then we can pass the arguements to the firebase API
-      setError("")
-      signup(emailRef.current.value, passwordRef.current.value)
 
-      //if the sign up is successful then we can redirect to the questionnarre
-      redirect.push("/questionnaire")
+    try {
+      //is all self check are working then we can pass the arguements to the firebase API
+      setError('');
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+
+      //if the sign up is successful then we can redirect to the questionnaire
+      redirect.push('/questionnaire');
+    } catch (e) {
+      setError('Failed to create account');
     }
-    catch (exception){
-      setError("Failed to create account")
-    }
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
@@ -63,7 +64,7 @@ const SignUp = () => {
       <div className='log-in-form'>
         <Form onSubmit={handleSignUp} className='form-group mb-2'>
           <h3 className='mb-2 text-center font-weight-bold'>Sign Up</h3>
-          {error && <Alert variant="danger">{error}</Alert>}
+          {error && <Alert variant='danger'>{error}</Alert>}
           <Form.Control
             type='email'
             className='form-control mb-2'
@@ -82,10 +83,14 @@ const SignUp = () => {
             placeholder='Confirm Password'
             ref={passwordConfirmRef}
           />
-          <button type='submit' disabled={loading} className='btn btn-secondary mt-3 mb-2 btn-block'>
+          <button
+            type='submit'
+            disabled={loading}
+            className='btn btn-secondary mt-3 mb-2 btn-block'
+          >
             Sign Up
           </button>
-          <div className="text-center">
+          <div className='text-center'>
             Already have an account? <Link to='/log-in'>Log In</Link>
           </div>
         </Form>
