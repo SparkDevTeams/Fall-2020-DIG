9882ca9730ffb3c472ac06cf0784069ff8c6b962
Ariadna Arias <ariascantero@gmail.com>
2020-11-13 21:57:02 -0500

    Fixed text and spacing of accordion

--- src/pages/SuccessPage.js
+++ src/pages/SuccessPage.js
@@ -31,12 +31,12 @@ const SuccessPage = () => {
       </div>
       <Accordion className='accordion-section'>
         <Card className='accordion-section'>
-          <Accordion.Toggle as={Card.Header} eventKey='0' className='mb-1'>
-            Energy Tips
+          <Accordion.Toggle as={Card.Header} eventKey='0'>
+            <h5>Energy Tips</h5>
           </Accordion.Toggle>
           <Accordion.Collapse eventKey='0' className='accordion-tips'>
             <Card.Body>
-              <h6>Switch lights off!</h6>
+              <h6>🍃 Switch lights off!</h6>
               <p>
                 When leaving the room turn off the lights and unplug electronic
                 devices when they are not in use.
@@ -45,7 +45,7 @@ const SuccessPage = () => {
           </Accordion.Collapse>
           <Accordion.Collapse eventKey='0' className='accordion-tips'>
             <Card.Body>
-              <h6>Lower your thermostat in winter and raise it in summer.</h6>
+              <h6>🍃 Lower your thermostat in winter and raise it in summer.</h6>
               <p>
                 Use less air conditioning in the summer; instead opt for fans,
                 which require less electricity. And check out these other ways
@@ -55,12 +55,12 @@ const SuccessPage = () => {
           </Accordion.Collapse>
         </Card>
         <Card className='accordion-section'>
-          <Accordion.Toggle as={Card.Header} eventKey='1' className='mb-1'>
-            Water Tips
+          <Accordion.Toggle as={Card.Header} eventKey='1'>
+            <h5>Water Tips</h5>
           </Accordion.Toggle>
           <Accordion.Collapse eventKey='1' className='accordion-tips'>
             <Card.Body>
-              <h6>Take shorter showers</h6>
+              <h6>🍃 Take shorter showers</h6>
               <p>
                 A typical shower uses five to ten gallons of water a minute.
                 Limit your showers to the time it takes to soap up, wash down
@@ -70,7 +70,7 @@ const SuccessPage = () => {
           </Accordion.Collapse>
           <Accordion.Collapse eventKey='1' className='accordion-tips'>
             <Card.Body>
-              <h6>Turn off the water while brushing your teeth</h6>
+              <h6>🍃 Turn off the water while brushing your teeth</h6>
               <p>
                 Before brushing, wet your brush and fill a glass for rinsing
                 your mouth.{' '}
@@ -80,11 +80,11 @@ const SuccessPage = () => {
         </Card>
         <Card className='accordion-section'>
           <Accordion.Toggle as={Card.Header} eventKey='2'>
-            Waste Tips
+            <h5>Waste Tips</h5>
           </Accordion.Toggle>
           <Accordion.Collapse eventKey='2' className='accordion-tips'>
             <Card.Body>
-              <h6>Reduce your food waste</h6>
+              <h6>🍃 Reduce your food waste</h6>
               <p>
                 Plan your meals ahead of time, freeze the excess and reuse
                 leftovers.
@@ -93,7 +93,7 @@ const SuccessPage = () => {
           </Accordion.Collapse>
           <Accordion.Collapse eventKey='2' className='accordion-tips'>
             <Card.Body>
-              <h6>Eat low on the food chain</h6>
+              <h6>🍃 Eat low on the food chain</h6>
               <p>
                 This means eating mostly fruits, veggies, grains, and beans.{' '}
               </p>
