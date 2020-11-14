4cc4e44ea690f8071e4c598d81bf93c092174758
Jorge <jorgenriosp@gmail.com>
2020-11-14 13:15:54 -0500

    Added more information

--- src/pages/Tips.js
+++ src/pages/Tips.js
@@ -4,75 +4,92 @@ import Card from 'react-bootstrap/Card';
 
 const Tips = () => {
   return (
-    <div className='tips-page-container'>
-      <div className='jumbotron jumbotron-fluid tips-jumbotron'>
-        <div className='container'>
-          <h1 className='display-2'>Tips</h1>
-          <p className='lead'>
-            Great tips for living a more eco friendly life.
+    <div className="tips-page-container">
+      <div className="jumbotron jumbotron-fluid tips-jumbotron">
+        <div className="container">
+          <h1 className="display-2">Tips</h1>
+          <p className="lead">
+            Your carbon footprint is the total amount of greehouse gas emissions
+            that come from the production (or use) of a product or service.
+            Think of how you chose to travel, the foods you consume, and how
+            much energy is being used in your home. Science is complex, and alot
+            of the time it can be overwhelming to understand what it all means.
+            Below are some great tips on how you can reduce your carbon
+            footprint and help our environment.
           </p>
         </div>
       </div>
-      <div className='container'>
-        <Accordion defaultActiveKey='0'>
+      <div className="container">
+        <Accordion defaultActiveKey="0">
           <Card>
-            <Accordion.Toggle as={Card.Header} eventKey='0'>
+            <Accordion.Toggle as={Card.Header} eventKey="0">
               Transportation
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey='0'>
+            <Accordion.Collapse eventKey="0">
               <Card.Body>
-                🍃 Reduce the amount of times you drive. Consider talking,
-                taking public transportation, carpooling or riding a bike to
-                your destination. This will reduce the amount of CO2 emitted and
-                lessens traffic congestion.
+                🍃 Driving less will reduce the amount of CO2 emitted and it
+                will lessen traffic congestion. Consider taking public
+                transportation, carpooling or riding a bike to your destination.
                 <br />
                 🍃 If you are driving, avoid making unnecesarry braking and
                 acceleration. Studies have found that aggressive driving can
                 result in more fuel consumption compared to consistent and calm
                 driving.
+                <br />
+                🍃 If you are going on a long trip, using cruise control can
+                reduce your fuel consumption.
               </Card.Body>
             </Accordion.Collapse>
           </Card>
 
           <Card>
-            <Accordion.Toggle as={Card.Header} eventKey='1'>
+            <Accordion.Toggle as={Card.Header} eventKey="1">
               Household
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey='1'>
+            <Accordion.Collapse eventKey="1">
               <Card.Body>
-                🍃 Consider using LED lightbulbs instead of the traditional
-                ones. LED bulbs may cost more but they use a quarter of the
-                energy of the incandescent ones and last up to 25 times longer.
+                🍃 Consider using LED lightbulbs instead of regular lightbulbs.
+                LED bulbs may cost more but they use a quarter of the energy
+                compared to the regular ones and last up to 25 times longer.
                 <br />
                 🍃 Make sure you turn off all electronic devices when not in use
                 and dont forget to turn off the light when leaving your room.
+                <br />
+                🍃 If you use the water heater often, try turning down the
+                heater to 120˚F. Doing this can save you roughly 500 pounds of
+                CO2.
               </Card.Body>
             </Accordion.Collapse>
           </Card>
 
           <Card>
-            <Accordion.Toggle as={Card.Header} eventKey='2'>
+            <Accordion.Toggle as={Card.Header} eventKey="2">
               Food
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey='2'>
+            <Accordion.Collapse eventKey="2">
               <Card.Body>
                 🍃 Reduce the amount of food you waste by planning your meals
                 ahead of time, freezing any leftovers, and reusing any leftovers
                 that could be left behind.
                 <br />
                 🍃 Consider eating more plant-based meals. Meat and dairy are
-                responsible for 14.5 percent of manmade global greenhouse gas
+                responsible for 14.5% of manmade global greenhouse gas
                 emissions. You can reduce your carbon footprint by consuming
                 more vegetables, fruits, and grains.
+                <br />
+                🍃 When doing groceries, choose organic and local foods that are
+                in season. Opting to purchase locally instead of transporting
+                foods from far away will reduce air pollution and cut back on
+                greenhouse emissions.
               </Card.Body>
             </Accordion.Collapse>
           </Card>
 
           <Card>
-            <Accordion.Toggle as={Card.Header} eventKey='3'>
+            <Accordion.Toggle as={Card.Header} eventKey="3">
               Shopping
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey='3'>
+            <Accordion.Collapse eventKey="3">
               <Card.Body>
                 🍃 Reduce the amount of stuff you purchase and consider using
                 recycled items when possible.
@@ -80,15 +97,19 @@ const Tips = () => {
                 🍃 If youre looking to purchase a new computer, try purchasing a
                 laptop instead of a traditional desktop. Dekstops require more
                 energy than laptops, making them less environmentally friendly.
+                <br />
+                🍃 Bring your own reusable bag when shopping around. This will
+                decrease the amount of plastic bags used, which can pollute our
+                environment.
               </Card.Body>
             </Accordion.Collapse>
           </Card>
 
           <Card>
-            <Accordion.Toggle as={Card.Header} eventKey='4'>
+            <Accordion.Toggle as={Card.Header} eventKey="4">
               Clothing
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey='4'>
+            <Accordion.Collapse eventKey="4">
               <Card.Body>
                 🍃 Washing your clothes in cold water could save up to 500
                 pounds of carbon dioxide. Using cold water instead of hot/warm
@@ -97,6 +118,10 @@ const Tips = () => {
                 🍃 Buying secondhand clothes can save the resources used to
                 otherwise create a new clothing piece. Shopping for second hand
                 items can also save you more money.
+                <br />
+                🍃 When buying new clothes, look for items that are made with
+                eco-friendly and natural fabrics. Some of them include, bamboo,
+                silk, hemp, and organic cotton.
               </Card.Body>
             </Accordion.Collapse>
           </Card>
